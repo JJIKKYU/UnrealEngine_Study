fe44c96bfc9ed83170ba22e49daf513b192adbaa
JIKKYU <jjinggu96@gmail.com>
2019-08-09 16:35:27 +0900

    4-9. 사용자 정의 액터 컴포넌트 생성

--- Projectfile/Intermediate/Build/Win64/UE4Editor/Inc/BasicStudy/BasicStudy.init.gen.cpp
+++ Projectfile/Intermediate/Build/Win64/UE4Editor/Inc/BasicStudy/BasicStudy.init.gen.cpp
@@ -21,8 +21,8 @@ void EmptyLinkFunctionForGeneratedCodeBasicStudy_init() {}
 				nullptr,
 				0,
 				PKG_CompiledIn | 0x00000000,
-				0x4F222073,
-				0x37C572F9,
+				0x5EEAB12F,
+				0x499BDC95,
 				METADATA_PARAMS(nullptr, 0)
 			};
 			UE4CodeGen_Private::ConstructUPackage(ReturnPackage, PackageParams);

59abf4f5e8d4f2393023f8c8d1564e7106d222f2
JJIKKYU <jjikkyu@naver.com>
2019-08-17 22:07:18 +0900

    8-16. 새 에디터 창 만들기

--- Projectfile/Source/BasicStudyEditor/BasicStudyEditor.h
+++ Projectfile/Source/BasicStudyEditor/BasicStudyEditor.h
@@ -20,7 +20,10 @@ public:
 
 	void MyButton_Clicked()
 	{
-		TSharedRef<SWindow> BasicStudyWindow = SNew(SWindow).Title(FText::FromString(TEXT("BasicStduy Window"))).ClientSize(FVector2D(800, 400)).SupportsMaximize(false).SupportsMinimize(false);
+		TSharedRef<SWindow> BasicStudyWindow = SNew(SWindow).Title(FText::FromString(TEXT("BasicStduy Window"))).ClientSize(FVector2D(800, 400)).SupportsMaximize(false).SupportsMinimize(false)
+			[
+				SNew(SVerticalBox)
+				+ SVerticalBox::Slot().HAlign(HAlign_Center).VAlign(VAlign_Center)[SNew(STextBlock).Text(FText::FromString(TEXT("Hello from Slate")))]];
 
 		IMainFrameModule& MainFrameModule = FModuleManager::LoadModuleChecked<IMainFrameModule>(TEXT("MainFrame"));
 
@@ -32,18 +35,18 @@ public:
 		{
 			FSlateApplication::Get().AddWindow(BasicStudyWindow);
 		}
-	};
+	}
 
 	void AddToolbarExtension(FToolBarBuilder& builder)
 	{
 		FSlateIcon IconBrush = FSlateIcon(FEditorStyle::GetStyleSetName(), "LevelEditor.ViewOptions", "LevelEditor.ViewOptions.Small");
 
 		builder.AddToolBarButton(FBasicStudyCommands::Get().MyButton, NAME_None, FText::FromString("My Button"), FText::FromString("Click me to display a message"), IconBrush, NAME_None);
-	};
+	}
 
 	void AddMenuExtension(FMenuBuilder& builder)
 	{
 		FSlateIcon IconBrush = FSlateIcon(FEditorStyle::GetStyleSetName(), "LevelEditor.ViewOptions", "LevelEditor.ViewOptions.Small");
 		builder.AddMenuEntry(FBasicStudyCommands::Get().MyButton);
-	};
+	}
 };
